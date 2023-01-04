0c3f96c511967f6a801e5a073b75c86850dae02b
EskoJuhani <eskojuhani@gmail.com>
2023-01-04 10:01:47 +0200

    dev

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
 
+inject();
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
